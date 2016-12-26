698e79a9e64efc23893d165dcf46d287ee041bb5
chandramanimadhav <chandramanimadhav@gmail.com>
2016-12-26 19:43:56 +0530

    Updated code to delete a node in link list.

--- Delete_a_node_using_switch_in_linked_list.cpp
+++ Delete_a_node_using_switch_in_linked_list.cpp
@@ -37,33 +37,48 @@
     }
     assert(0);              
 }
- void deleteNode(struct node *node_ptr,int index)
- {
-      struct node *temp = node_ptr->next;
-      node_ptr->data = temp->data;
-      node_ptr->next = temp->next;
-      free(temp);
- }
- int main()
- {
-     struct node* head = NULL;
-     int index;
-     push(&head,4);
-     push(&head,3);
-     push(&head,2);
-     push(&head,1);
-     printf("\nBefore deleting linked list is: ");
-     printList(head); 
-     for(index = 0; index <= 3; index++)
-     {
-     	if( index==2)
-     	{
-     		current =index != NULL;
-     		current = curent->next;
-			deleteNode(head);
-        	printf("\nDelete the index: ");
-   	    	printList(head);
-     	}
-     }     
-     getch();
- }
+void deleteNode(struct node **head_ptr, int index) {
+    struct node *temp = (*head_ptr);
+    struct node *prev_node, *next_node, *current_node;
+    prev_node = NULL;
+    next_node = NULL;
+    current_node = NULL;
+    int count = 0;
+    while (temp != NULL) {
+        current_node = temp;
+        next_node = temp->next;
+        if(count == index) {
+            break;
+        } else {
+            prev_node = temp;
+            next_node = NULL;
+            current_node = NULL;
+            count++;
+        }
+        temp = temp->next;
+    }
+    if (current_node != NULL) {
+        if (prev_node != NULL) {
+            prev_node->next = next_node;
+        } else {
+            (*head_ptr) = next_node;
+        }
+        free(current_node);
+    } else {
+        printf("\nIndex out ot boundary.");
+    }
+}
+int main() {
+	struct node* head = NULL;
+	int index;
+	push(&head,4);
+	push(&head,3);
+	push(&head,2);
+	push(&head,1);
+	printf("\nBefore deleting linked list is: ");
+	printList(head);
+	printf("Enter index to be delete : ");
+	scanf("%d", &index);
+    deleteNode(&head, index);
+    getch();
+}
