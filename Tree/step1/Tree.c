464805fa8ed34b69dd0f62c238ac91a6f554e069
chandramanimadhav <chandramanimadhav@gmail.com>
2017-01-20 19:27:47 +0530

    Added problem statement.

--- Tree/step1/Tree.c
+++ Tree/step1/Tree.c
@@ -1,1 +1,17 @@
+/*
+Write a c programm :
 
+Create a binary search tree by asking 5 numbers from users one at a time.
+Print this tree in in-order, pre-order and post-order.
+Give user option to insert a number in the given BST.
+Again print in in-order, pre-order and post-order.
+Ask a number to user to search in a tree.
+Ask a number from user to delete this number if it exist in a tree otherwise say not found.
+Again print in in-order, pre-order and post-order.
+
+
+Hint:
+
+Use switch statement.
+
+*/
